a0de5175f9cf0c12409b6a1a9511485e38d5a1f7
marjon <marjon@asti.dost.gov.ph>
2021-10-25 21:37:48 +0800

    Add user model
            - Generate user model using sequelize-auto ( cli for sequelize )
            (https://github.com/sequelize/sequelize-auto)
            - Install sequelize and mysql2

---
+++ app/models/user.js
@@ -0,0 +1,57 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('user', {
+    id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    first_name: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    last_name: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    address: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    postcode: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    contact_number: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    username: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    password: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    tableName: 'user',
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id" },
+        ]
+      },
+    ]
+  });
+};
