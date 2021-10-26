0d51f83d8e382f37fb66226fd4b750519fc7705e
marjon <marjon@asti.dost.gov.ph>
2021-10-26 08:35:04 +0800

    Add GET for single user
            - /users/<userId>

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -11,44 +11,51 @@ module.exports = function(model) {
                 if(users){
                     res.json(users);
                 } else { 
-                    res.status(404);
+                    res.status(404).json('No users found');
                 }
 
             } catch (e){
-                res.status(500);
+                console.error(e)
+                res.status(500).json('Something whent wrong');
+            }
+        },
+
+        get_user: async function(req, res) {
+            try{
+                let user = await model.findByPk(req.params.userId)
+
+                console.log(user);
+                if(user){
+                    res.json(user)
+                } else {
+                    res.status(404).json('Page not found');
+                }
+
+            } catch (e) {
+                console.error(e)
+                res.status(500).json('Something when wrong');
+            }
+        },
+
+        create_user: async function(req, res) {
+            try {
+            } catch(e) {
+            }
+        },
+
+        update_user: async function(req, res) {
+            try {
+
+            } catch(e) {
+            }
+        },
+
+        delete_user: async function(req, res) {
+            try {
+            } catch(e) {
             }
         }
+
     }
 
 }
-
-
-exports.list_all_users = async function(req, res) {
-    let users = await models.user.findAll()
-
-    if(result){  
-        res.json(users);
-    } else { 
-        res.status(404);
-    }
-
-};
-
-exports.create_user = function(req, res) {
-    res.json(true);
-};
-
-
-exports.get_user = function(req, res) {
-    res.json(true);
-};
-
-
-exports.update_user = function(req, res) {
-    res.json(true);
-};
-
-
-exports.delete_user = function(req, res) {
-    res.json(true);
-};
--- app/routes/UserRoute.js
+++ app/routes/UserRoute.js
@@ -5,12 +5,11 @@ module.exports = function(app, models) {
     // todoList Routes
     app.route('/users')
         .get(controller.list_all_users);
-/*
-        .post(controller.create_user);
+//        .post(controller.create_user);
 
-
-    app.route('/tasks/:taskId')
+    app.route('/users/:userId')
         .get(controller.get_user)
+/*
         .put(controller.update_user)
         .delete(controller.delete_user);
 */
