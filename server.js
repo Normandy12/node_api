84453b3ca31727db96f5a7972593ef483da4d536
marjon <marjon@asti.dost.gov.ph>
2021-10-26 08:48:35 +0800

    Install body-parser
            - For parsing request body

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var initModels = require("./app/models/init-models");
 var express = require('express'),
     app = express(),
     port = 3000;
+    bodyParser = require('body-parser');
 
 const sequelize = new Sequelize('node_api', process.env.DEV_USER, process.env.DEV_PASS, {
     host: 'localhost',
@@ -13,6 +14,9 @@ const sequelize = new Sequelize('node_api', process.env.DEV_USER, process.env.DE
 });
 
 var models = initModels(sequelize)
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 var routes = require('./app/routes/UserRoute');
 routes(app,models);
